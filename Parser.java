1c4d99261131cb27de3059cc327922a792556650
Vincas Turskis <v.turskis@lancaster.ac.uk>
2024-03-05 01:17:31 +0100

    Solved task 0, maybe

--- Parser.java
+++ Parser.java
@@ -2,29 +2,11 @@ public class Parser extends AbstractParser {
 // your code goes below
 	public float parseStart(){
 		/*Grammar in LL(1) form:
-		<start> ::= <Bracket> | <Number>
-		<Number> ::= "number" <NumberFollow>
-		<NumberFollow> ::= <Operator> | epsilon
-		<Operator> ::= "+" <Number> | - <Number> | + <Number>
-		<OperatorFollow> ::=
-		<Bracket> ::= "(" <openBracketFollow>
-		<openBracketFollow> ::= <lastOpenBracket> | <lastNumber> ")" 
-
-
-
-		<start> ::= <factor><B>
-		<B> ::= "*" <factor> <B> | "+" <factor> <B> | "-" <factor> <B> | epsilon
-		<factor> ::= "(" <start> ")" | float
-
-
-
-
-
-
-		<start> ::= <start> <rem> | <term>
-		<rem> ::= "+" <term> | "-" <term>
-		<term> ::= <term> "*" <factor> | <factor>
-		<factor> ::= "(" <start> ")" | <float>
+		<start> ::= <anyNext>
+		<anyNext> ::= <brackets> | <numExpression>
+		<brackets> ::= "(" <numExpression> ")"
+		<numExpression> ::= "float" <numFollow>
+		<numFollow> ::= "*" <anyNext> | "+" <anyNext> | "-" <anyNext>
 		*/
 		Lexer lexer = new Lexer();
 		String curToken = lexer.getNextToken();
