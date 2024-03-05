1a3e9dac36264baaaafc7e4711f8b984a14173b7
Vincas Turskis <v.turskis@lancaster.ac.uk>
2024-03-05 10:17:59 +0100

    Implemented task 1

--- Parser.java
+++ Parser.java
@@ -2,18 +2,18 @@ public class Parser extends AbstractParser {
 	/*Grammar in LL(1) form:
 	<start> ::= <anyNext>
 	<anyNext> ::= <brackets> | <numExpression>
-	<brackets> ::= "(" <numExpression> ")"
+	<brackets> ::= "(" <anyNext> ")" <numFollow>
 	<numExpression> ::= "float" <numFollow>
-	<numFollow> ::= "*" <anyNext> | "+" <anyNext> | "-" <anyNext>
+	<numFollow> ::= "*" <anyNext> | "+" <anyNext> | "-" <anyNext> | epsilon
 	*/
 	public float parseStart(){
 
-		boolean result = EvaluateStart(curTok);
+		boolean result = Evaluate();
 		if(result == true)
 		{
-			curTok = lex.getNextToken();
 			if(!curTok.equals("$"))
 			{
+				System.out.println("Found unexpected token");
 				return -1;
 			}
 			return 1;
@@ -24,64 +24,79 @@ public class Parser extends AbstractParser {
 		}
 	}
 
-	private boolean EvaluateStart(String token)
+	private boolean Evaluate()
 	{
-		return EvaluateAnyNext(token);
+		return EvaluateAnyNext();
 	}
-	private boolean EvaluateAnyNext(String token)
+	private boolean EvaluateAnyNext()
 	{
-		if(token.equals("("))
+		if(curTok.equals("("))
 		{
 			System.out.println("Found opening bracket");
-			return EvaluateBrackets(token);
+			return EvaluateBrackets();
 		}
 		else
 		{
-			return EvaluateNumExpression(token);
+			return EvaluateNumExpression();
 		}
 	}
-	private boolean EvaluateBrackets(String token)
+	private boolean EvaluateBrackets()
 	{
-		if(!token.equals("("))
+		if(!curTok.equals("("))
 		{
 			System.out.println("Should be opening bracket");
 			return false;
 		} 
-		token = lex.getNextToken();
-		if(!EvaluateNumExpression(token))
+		curTok = lex.getNextToken();
+		if(!EvaluateAnyNext())
 		{
 			return false;
 		}
-		token = lex.getNextToken();
-		if(!token.equals(")"))
+		if(!curTok.equals(")"))
 		{
 			System.out.println("Missing closing bracket");
 			return false;
 		}
 		System.out.println("Found closing bracket");
-		return true;
+		curTok = lex.getNextToken();
+		return EvaluateNumFollow();
 	}
-	private boolean EvaluateNumExpression(String token)
+	private boolean EvaluateNumExpression()
 	{
 		try {
-			Float.parseFloat(token);
+			Float.parseFloat(curTok);
 		} catch (Exception e) {
 			System.out.println("Failed to parse float");
 			return false;
 		}
 		System.out.println("Found number");
-		token = lex.getNextToken();
-		return EvaluateNumFollow(token);
+		curTok = lex.getNextToken();
+		return EvaluateNumFollow();
 	}
-	private boolean EvaluateNumFollow(String token)
+	private boolean EvaluateNumFollow()
 	{
-		if(!token.equals("-") && !token.equals("+") && !token.equals("*"))
-		{
-			System.out.println("Missing operator");
-			return false;
+		switch (curTok) {
+			case "-":
+				System.out.println("found \"-\" operator");
+				curTok = lex.getNextToken();
+				return EvaluateAnyNext();
+			case "+":
+				System.out.println("found \"+\" operator");
+				curTok = lex.getNextToken();
+				return EvaluateAnyNext();
+			case "*":
+				System.out.println("found \"*\" operator");
+				curTok = lex.getNextToken();
+				return EvaluateAnyNext();
+			case ")":
+				System.out.println("epsilon");
+				return true;
+			case "$":
+				System.out.println("eof");
+				return true;
+			default:
+				System.out.println("Incorrect num follow token");
+				return false;
 		}
-		System.out.println("Found operator");
-		token = lex.getNextToken();
-		return EvaluateAnyNext(token);
 	}
 }
