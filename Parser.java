eac9411d6d1f5287275ee0201858c5a3c1f55b08
Vincas Turskis <v.turskis@lancaster.ac.uk>
2024-03-05 11:10:26 +0100

    Implemented version with while loop

--- Parser.java
+++ Parser.java
@@ -2,11 +2,9 @@ import java.text.ParseException;
 
 public class Parser extends AbstractParser {
 	/*Grammar in LL(1) form:
-	<start> ::= <anyNext>
-	<anyNext> ::= <brackets> | <numExpression>
-	<brackets> ::= "(" <anyNext> ")" <numFollow>
-	<numExpression> ::= "float" <numFollow>
-	<numFollow> ::= "*" <anyNext> | "+" <anyNext> | "-" <anyNext> | epsilon
+	<start> ::= <term> { "+" <term> | "-" <term }
+	<term> ::= <factor> { "*" <factor> }
+	<factor> ::= "(" <start> ")" | float
 	*/
 	public float parseStart(){
 		float result = -1;
@@ -26,64 +24,72 @@ public class Parser extends AbstractParser {
 
 	private float Evaluate() throws ParseException
 	{
-		return EvaluateAnyNext();
+		return EvaluateStart();
 	}
-	private float EvaluateAnyNext() throws ParseException
+	private float EvaluateStart() throws ParseException
 	{
+		float result = 0;
+		result += EvaluateTerm();
+		while(curTok.equals("+") || curTok.equals("-"))
+		{
+			switch (curTok) {
+				case "+":
+					curTok = lex.getNextToken();
+					result += EvaluateTerm();
+					break;
+				case "-":
+					curTok = lex.getNextToken();
+					result -= EvaluateTerm();
+					break;
+				default:
+					throw new ParseException("Error: expected an operator", 0);
+			}
+		}
+		return result;
+	}
+	private float EvaluateTerm() throws ParseException
+	{
+		float result = 0;
+		result += EvaluateFactor();
+		while(curTok.equals("*"))
+		{
+			curTok = lex.getNextToken();
+			result *= EvaluateFactor();
+		}
+		return result;
+		
+	}
+	private float EvaluateFactor() throws ParseException
+	{
+		float result;
+		float parsed = 0;
+		boolean foundFloat = false;
+		try {
+			parsed = Float.parseFloat(curTok);
+			foundFloat = true;
+		} catch (Exception e) {
+			foundFloat = false;
+		}
+		if(foundFloat)
+		{
+			curTok = lex.getNextToken();
+			return parsed;
+		}
 		if(curTok.equals("("))
 		{
-			return EvaluateBrackets();
+			curTok = lex.getNextToken();
+			result = EvaluateStart();
+			if(!curTok.equals(")"))
+			{
+				throw new ParseException("Error: expected closing bracked, found " + curTok, 0);
+			}
+			curTok = lex.getNextToken();
+			return result;
 		}
 		else
 		{
-			return EvaluateNumExpression();
+			throw new ParseException("Error: expected opening bracket or number, found " + curTok, 0);
 		}
-	}
-	private float EvaluateBrackets() throws ParseException
-	{
-		float result = 0;
-		if(!curTok.equals("("))
-		{
-			throw new ParseException("Error: Expected opening bracket, found " + curTok, 0);
-		} 
-		curTok = lex.getNextToken();
-		result = EvaluateAnyNext();
-		if(!curTok.equals(")"))
-		{
-			throw new ParseException("Error: Expected closing bracket, found " + curTok, 0);
-		}
-		curTok = lex.getNextToken();
-		return EvaluateNumFollow(result);
-	}
-	private float EvaluateNumExpression() throws ParseException
-	{
-		float result = 0;
-		try {
-			result = Float.parseFloat(curTok);
-		} catch (Exception e) {
-			throw new ParseException("Error: Expected floating point expression, but could not parse " + curTok + " as float.", 0);
-		}
-		curTok = lex.getNextToken();
-		return EvaluateNumFollow(result);
-	}
-	private float EvaluateNumFollow(float originNumber) throws ParseException
-	{
-		switch (curTok) {
-			case "-":
-				curTok = lex.getNextToken();
-				return (originNumber - EvaluateAnyNext());
-			case "+":
-				curTok = lex.getNextToken();
-				return (originNumber + EvaluateAnyNext());
-			case "*":
-				curTok = lex.getNextToken();
-				return (originNumber * EvaluateAnyNext());
-			case ")":
-				return originNumber;
-			case "$":
-				return originNumber;
-			default:
-				throw new ParseException("Error: A number should be followed by an operator, closing bracket or EOF; Found " + curTok, 0);
-		}
+
 	}
 }
